580292f1859ad3c4a5a32909648cf87bcdfbf650
Muhammed Riyas <muhammedriyas0092@gmail.com>
2026-06-15 16:27:07 +0530

    refactor(admin): separate validation schemas and use in routes

--- server/src/modules/admin/entry-points/routes/Auth.routes.ts
+++ server/src/modules/admin/entry-points/routes/Auth.routes.ts
@@ -1,12 +1,15 @@
 import asyncHandler from '../../../../shared/utils/AsyncHandler';
 import { CustomLimit } from '../../../../shared/utils/RateLimiter';
 import { adminAuthContainer } from '../dependencyInjection/AdminAuthContainer';
+import { validateRequest } from '../../../../shared/middlewares/validateRequest';
+import { LoginAdminSchema } from '../validations/AdminAuthValidation';
 import { Router } from 'express';
 const router = Router();
 
 router.post(
   '/login',
   CustomLimit(10, 'login'),
+  validateRequest(LoginAdminSchema),
   asyncHandler(adminAuthContainer.login),
 );
 
